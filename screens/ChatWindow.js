b4564d13f5dbe909c01120dae392b3f858daac76
Bhupendra sambare <bhupendrasam1404@gmail.com>
2024-01-13 22:20:09 +0530

    navigation change

--- screens/ChatWindow.js
+++ screens/ChatWindow.js
@@ -2,13 +2,15 @@ import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native'
 import React, { useState } from 'react'
 import ChatNavbar from '../components/ChatNavbar';
 import { getSubStringByLength } from '../helpers/helper';
+import { useNavigation } from '@react-navigation/native';
 
-const Chat = () => {
+const ChatWindow = () => {
 
     const [isMessage,setIsMessage] = useState(true);
     const messages = require('../helpers/message.json');
     const users = require('../helpers/users.json');
-    
+    const navivation = useNavigation();
+
   return (
     <View className="bg-white">
         <ChatNavbar users={users}/>
@@ -27,7 +29,7 @@ const Chat = () => {
         <ScrollView showsVerticalScrollIndicator={false} className="h-full ">
             
             {messages.map((msg) => (
-                <TouchableOpacity>
+                <TouchableOpacity onPress={()=>navivation.navigate("MESSAGE")}>
                     <View className="flex-row my-2 mx-3 justify-between">
                         <View className="flex-row my-2">
                             <Image
@@ -38,7 +40,7 @@ const Chat = () => {
                             <View className="flex mb-2 mx-2 justify-center">
                                 <Text className="text-lg">{msg.user.fullName}</Text>
                                 <Text className="text-md text-slate-500">
-                                    {getSubStringByLength(msg.messages[0].message,40)}
+                                    {getSubStringByLength(msg.messages[msg.messages.length-1].message,40)}
                                 </Text>
                             </View>
                         </View>
@@ -52,4 +54,4 @@ const Chat = () => {
   )
 }
 
-export default Chat
+export default ChatWindow
---
+++ screens/Message.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Message = () => {
+  return (
+    <View>
+      <Text>Chat</Text>
+    </View>
+  )
+}
+
+export default Message
